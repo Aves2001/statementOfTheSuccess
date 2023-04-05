712375bc24805d4f8ea87c9394b6fae650d22a7d
Aves2001 <lebid2001@gmail.com>
2023-04-05 23:06:25 +0300

    fix save GroupAdminForm

--- statementOfTheSuccess/main/forms.py
+++ statementOfTheSuccess/main/forms.py
@@ -22,7 +22,7 @@ class GroupAdminForm(ModelForm):
         model = Group
         exclude = []
 
-    group = ModelMultipleChoiceField(
+    speciality = ModelMultipleChoiceField(
         queryset=Group.objects.all(),
         required=False,
         widget=FilteredSelectMultiple(verbose_name='Групи', is_stacked=False)
@@ -31,15 +31,24 @@ class GroupAdminForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(GroupAdminForm, self).__init__(*args, **kwargs)
         if self.instance.pk:
-            self.fields['group'].initial = Group.objects.filter(speciality=self.instance.pk)
+            self.fields['speciality'].initial = Group.objects.filter(speciality=self.instance.pk)
 
     def save_m2m(self):
         pass
+    #     # if self.instance.pk:
+    #     #     self.cleaned_data['group'].update(speciality=None)
+    #     #     self.fields['group'].initial.update(speciality=None)
 
+    #
     def save(self, *args, **kwargs):
         instance = super(GroupAdminForm, self).save()
+
         if self.instance.pk:
-            self.cleaned_data['group'].update(speciality=instance.pk)
-            self.fields['group'].initial.update(speciality=instance.pk)
+            self.fields['speciality'].initial.update(speciality=instance.pk)
+            self.cleaned_data['speciality'].update(speciality=instance.pk)
+
+            qs = self.fields['speciality'].initial.difference(self.cleaned_data['speciality'])
+            for i in qs:
+                self.fields['speciality'].initial.filter(id=i.id).update(speciality=None)
         self.save_m2m()
         return instance
