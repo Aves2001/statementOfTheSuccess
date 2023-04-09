5b2031c791b73cb52d7d551b0d055e5a20ec5024
Aves2001 <lebid2001@gmail.com>
2023-04-10 00:49:56 +0300

    update urls

--- statementOfTheSuccess/main/urls.py
+++ statementOfTheSuccess/main/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 # todo
+
 urlpatterns = [
     path('', views.index, name='home'),
-    path('records', views.RecordList.as_view(), name='records'),
-    path('add-record', views.index, name='add-record'),
+    path('record', views.RecordList.as_view(), name='record'),
+    path('add-record', views.AddRecord.as_view(), name='add-record'),
+    path('profile', views.Profile.as_view(), name='profile'),
+    path('record-detal/<int:pk>/', views.RecordDetail.as_view(), name='record-detail'),
 ]
