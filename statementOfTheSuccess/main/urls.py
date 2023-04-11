aa8d1835f7718a9f1a4a30bb0df12edc7b4031ab
Aves2001 <lebid2001@gmail.com>
2023-04-11 23:45:19 +0300

    add url login and logout

--- statementOfTheSuccess/main/urls.py
+++ statementOfTheSuccess/main/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.contrib import auth
+from django.urls import path, include
 from . import views
 
 # todo
-
 urlpatterns = [
     path('', views.index, name='home'),
+    path('profile', views.Profile.as_view(), name='profile'),
+    # path("accounts/", include("django.contrib.auth.urls")),
+    path('login', views.LoginUser.as_view(), name='login'),
+    path('logout', views.LogoutUser.as_view(), name='logout'),
     path('record', views.RecordList.as_view(), name='record'),
     path('add-record', views.AddRecord.as_view(), name='add-record'),
     path('profile', views.Profile.as_view(), name='profile'),
