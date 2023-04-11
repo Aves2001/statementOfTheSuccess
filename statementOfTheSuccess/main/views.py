f6d26e1561f077426a19488b132bc848924d5ffc
Aves2001 <lebid2001@gmail.com>
2023-04-11 23:49:38 +0300

    add LoginUser

--- statementOfTheSuccess/main/views.py
+++ statementOfTheSuccess/main/views.py
@@ -1,33 +1,53 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+
+from .forms import UserLoginForm
 from .models import Record, Grade, Teacher
+
+
+class LoginUser(LoginView):
+    form_class = UserLoginForm
+    template_name = 'registration/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class LogoutUser(LogoutView):
+    pass
 
 
 def index(request):
     return render(request, 'main/index.html')
 
 
-class RecordList(ListView):
+class RecordList(LoginRequiredMixin, ListView):
     model = Record
     template_name = 'main/record.html'
     context_object_name = 'record'
     extra_context = {'title': 'Відомості'}
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['grade'] = Grade.objects.all()
-    #     return context
+    # TODO права
+    def get_queryset(self):
+        return Record.objects.filter(teacher=self.request.user.pk)
 
 
-class RecordDetail(DetailView):
+class RecordDetail(ListView):
     model = Grade
     template_name = 'main/record-detail.html'
     context_object_name = 'grade'
     extra_context = {'title': 'Відомість'}
 
     def get_queryset(self):
-        return Grade.objects.filter(record=Record.pk)
+        return Grade.objects.filter(record=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['record'] = Record.objects.filter(pk=self.kwargs['pk'])
+        return context
 
 
 class AddRecord(ListView):
