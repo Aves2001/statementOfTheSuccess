bf7854ed6352bf18e28f7b9343c045fe500901f5
Aves2001 <lebid2001@gmail.com>
2023-04-13 12:29:23 +0300

    fix urls

--- statementOfTheSuccess/main/urls.py
+++ statementOfTheSuccess/main/urls.py
@@ -6,11 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('profile', views.Profile.as_view(), name='profile'),
-    # path("accounts/", include("django.contrib.auth.urls")),
-    path('login', views.LoginUser.as_view(), name='login'),
+    path('login/', views.LoginUser.as_view(), name='login'),
     path('logout', views.LogoutUser.as_view(), name='logout'),
-    path('record', views.RecordList.as_view(), name='record'),
-    path('add-record', views.AddRecord.as_view(), name='add-record'),
-    path('profile', views.Profile.as_view(), name='profile'),
+    path('record/', views.RecordList.as_view(), name='record'),
+    path('add-record/', views.AddRecord.as_view(), name='add-record'),
     path('record-detal/<int:pk>/', views.RecordDetail.as_view(), name='record-detail'),
 ]
